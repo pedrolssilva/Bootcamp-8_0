1ad676f6500e1240eb2b472719844ecf8069783c
pedrolssilva <pedrolssilva@hotmail.com>
2019-09-13 21:33:18 -0300

    config and install nodemon, sucrase, prettier, eslint and editorConfig to standardize the code

--- F2-NodeJS/Iniciando_back-end_do_GoBarber/desafio2/src/routes.js
+++ F2-NodeJS/Iniciando_back-end_do_GoBarber/desafio2/src/routes.js
@@ -1,9 +1,7 @@
-const { Router } = require("express");
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello world" });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
 
-module.exports = routes;
+export default routes;
