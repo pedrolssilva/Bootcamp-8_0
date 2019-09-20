a0c07e94a3b929501aaaf863ec6b5edc9c151542
pedrolssilva <pedrolssilva@hotmail.com>
2019-09-19 21:03:09 -0300

    created route, model and controller to list providers and included avatar relationship to become url to file accessible

--- F2-NodeJS/ContinuandoAPIdoGoBarber/modulo3/src/routes.js
+++ F2-NodeJS/ContinuandoAPIdoGoBarber/modulo3/src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,7 +16,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-
+routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
