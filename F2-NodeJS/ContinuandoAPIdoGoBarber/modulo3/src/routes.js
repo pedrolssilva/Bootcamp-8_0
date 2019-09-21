a44263336d590c97546fa0705f36be39e4c94c3b
pedrolssilva <pedrolssilva@hotmail.com>
2019-09-20 22:20:13 -0300

    created route to list appointments by used logged

--- F2-NodeJS/ContinuandoAPIdoGoBarber/modulo3/src/routes.js
+++ F2-NodeJS/ContinuandoAPIdoGoBarber/modulo3/src/routes.js
@@ -18,6 +18,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
