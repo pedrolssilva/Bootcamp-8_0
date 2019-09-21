f40b1e8274523699c694eeb3e2d82b503f0bd0e5
pedrolssilva <pedrolssilva@hotmail.com>
2019-09-20 23:11:33 -0300

    created routine to list schedule by date query param

--- F2-NodeJS/ContinuandoAPIdoGoBarber/modulo3/src/routes.js
+++ F2-NodeJS/ContinuandoAPIdoGoBarber/modulo3/src/routes.js
@@ -7,6 +7,7 @@ import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,10 +17,10 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/appointments', AppointmentController.store);
+routes.put('/users', UserController.update);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/providers', ProviderController.index);
-routes.post('/files', upload.single('file'), FileController.store);
-
+routes.get('/schedule', ScheduleController.index);
 export default routes;
