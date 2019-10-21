44adf747376d89c9d6099ea16340622a8c038e7c
pedrolssilva <pedrolssilva@hotmail.com>
2019-10-21 18:45:48 -0300

    created controller and route to list all organizer's meetup

--- F2-NodeJS/ContinuandoAPIdoGoBarber/desafio3/src/routes.js
+++ F2-NodeJS/ContinuandoAPIdoGoBarber/desafio3/src/routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import multer from 'multer';
+import multerConfig from './config/multer';
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
-import authMiddleware from './app/middlewares/auth';
-import multerConfig from './config/multer';
+import OrganizerController from './app/controllers/OrganizerController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,5 +20,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:meetupId', MeetupController.update);
 routes.put('/users', UserController.update);
+routes.get('/organizer/meetups', OrganizerController.index);
 
 export default routes;
