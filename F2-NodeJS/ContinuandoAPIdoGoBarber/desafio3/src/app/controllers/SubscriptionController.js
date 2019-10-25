ebfb724f915611860095abfd776a82ac22db0846
pedrolssilva <pedrolssilva@hotmail.com>
2019-10-25 20:23:17 -0300

    setting and send e-mail in new subscription with handlebars templates

--- F2-NodeJS/ContinuandoAPIdoGoBarber/desafio3/src/app/controllers/SubscriptionController.js
+++ F2-NodeJS/ContinuandoAPIdoGoBarber/desafio3/src/app/controllers/SubscriptionController.js
@@ -4,10 +4,17 @@ import Meetup from '../models/Meetup';
 import Subscription from '../models/Subscription';
 import Notification from '../schemas/notification';
 import User from '../models/User';
+import mail from '../../lib/mail';
 
 class SubscriptionController {
   async store(req, res) {
-    const meetup = await Meetup.findByPk(req.params.meetupId);
+    const meetup = await Meetup.findByPk(req.params.meetupId, {
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['name', 'email'],
+      },
+    });
 
     if (!meetup) {
       return res.status(404).json({ error: 'meetup not found' });
@@ -60,6 +67,18 @@ class SubscriptionController {
     await Notification.create({
       content: `Nova inscrição de ${user.name} no meetup "${meetup.title}" do ${formattedDate}`,
       user: meetup.user_id,
+    });
+
+    await mail.sendMail({
+      to: `${meetup.user.name} <${meetup.user.email}>`,
+      subject: `Nova inscrição no meetup ${meetup.title}`,
+      template: 'subscription',
+      context: {
+        organizer: meetup.user.name,
+        meetupTitle: meetup.title,
+        userName: user.name,
+        date: formattedDate,
+      },
     });
 
     const subscription = await Subscription.create({
---
+++ F2-NodeJS/ContinuandoAPIdoGoBarber/desafio3/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2f315d0a5ab951',
+    pass: 'e682451e3510a5',
+  },
+  default: {
+    from: 'Equipe Meetapp <noreply@meetapp.com>',
+  },
+};
