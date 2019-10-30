c695c7c88662297ec99ed1d2555e3405590fb761
pedrolssilva <pedrolssilva@hotmail.com>
2019-10-29 21:55:28 -0300

    created first react component

--- F3-React/Introducao_ao_React/modulo4/src/index.js
+++ F3-React/Introducao_ao_React/modulo4/src/index.js
@@ -1,2 +1,5 @@
-const soma = (a, b) => a + b;
-alert(soma(1, 4));
+import React from "react";
+import { render } from "react-dom";
+import App from "./app";
+
+render(<App />, document.getElementById("app"));
