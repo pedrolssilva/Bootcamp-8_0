5ec65e331c34c6a3563af1afb1b7c43d922c28cd
pedrolssilva <pedrolssilva@hotmail.com>
2019-11-21 22:07:46 -0300

    handle error with try/catch in handleSubmit function

--- F3-React/Primeiro_projeto_com_React/modulo05/src/pages/Main/index.js
+++ F3-React/Primeiro_projeto_com_React/modulo05/src/pages/Main/index.js
@@ -12,6 +12,7 @@ export default class Main extends Component {
       newRepo: '',
       repositories: [],
       loading: false,
+      error: null,
     };
   }
 
@@ -39,24 +40,28 @@ export default class Main extends Component {
     e.preventDefault();
 
     this.setState({ loading: true });
-
     const { newRepo, repositories } = this.state;
 
-    const response = await api.get(`repos/${newRepo}`);
+    try {
+      const response = await api.get(`repos/${newRepo}`);
+      const data = {
+        name: response.data.full_name,
+      };
 
-    const data = {
-      name: response.data.full_name,
-    };
-
-    this.setState({
-      repositories: [...repositories, data],
-      newRepo: '',
-      loading: false,
-    });
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        error: null,
+      });
+    } catch (error) {
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
-    const { newRepo, loading, repositories } = this.state;
+    const { newRepo, loading, repositories, error } = this.state;
     return (
       <Container>
         <h1>
@@ -64,7 +69,7 @@ export default class Main extends Component {
           Repositórios
         </h1>
 
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={this.handleSubmit} error={error}>
           <input
             type="text"
             placeholder="Adicionar repositório"
