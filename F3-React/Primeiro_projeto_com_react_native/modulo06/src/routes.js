dd1f37e8a5de366776aa58d65d52921c3fc66c61
pedrolssilva <pedrolssilva@hotmail.com>
2019-12-05 22:55:17 -0300

    included informations and style in header navigation

--- F3-React/Primeiro_projeto_com_react_native/modulo06/src/routes.js
+++ F3-React/Primeiro_projeto_com_react_native/modulo06/src/routes.js
@@ -3,6 +3,20 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Main from './pages/Main';
 import User from './pages/User';
 
-const Routes = createAppContainer(createStackNavigator({ Main, User }));
+const Routes = createAppContainer(
+  createStackNavigator(
+    { Main, User },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#fff',
+      },
+    }
+  )
+);
 
 export default Routes;
