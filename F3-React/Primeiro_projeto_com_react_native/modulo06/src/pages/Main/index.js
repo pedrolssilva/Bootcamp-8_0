dfe20d7b741b81fe3d74481e91d68f949c9999e8
pedrolssilva <pedrolssilva@hotmail.com>
2019-12-11 22:01:11 -0300

    showing loading in button submit while is requesting info to api

--- F3-React/Primeiro_projeto_com_react_native/modulo06/src/pages/Main/index.js
+++ F3-React/Primeiro_projeto_com_react_native/modulo06/src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   Container,
@@ -22,10 +22,13 @@ export default class Main extends Component {
     this.state = {
       newUser: '',
       users: [],
+      loading: false,
     };
   }
 
   handleAddUser = async () => {
+    this.setState({ loading: true });
+
     const { users, newUser } = this.state;
 
     const response = await api.get(`/users/${newUser}`);
@@ -40,13 +43,14 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
     return (
       <Container>
         <Form>
@@ -59,8 +63,12 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#fff" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
 
