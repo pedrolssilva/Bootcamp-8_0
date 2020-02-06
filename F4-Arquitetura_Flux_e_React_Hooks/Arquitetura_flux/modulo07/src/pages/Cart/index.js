b31a5a3c4dda0a0ff520f9a2b64abe60955ed324
pedrolssilva <pedrolssilva@hotmail.com>
2020-02-05 21:10:56 -0300

    Refactored actions in home and cart, separating it in files into the cart reducer folder and used bindActionCreators from redux to inject actions dependencies into components

--- F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/pages/Cart/index.js
+++ F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {
@@ -7,8 +8,9 @@ import {
   MdDelete,
 } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
+import * as CartActions from '../../store/modules/cart/actions';
 
-function Cart({ cart, dispatch }) {
+function Cart({ cart, removeFromCart }) {
   return (
     <Container>
       <ProductTable>
@@ -48,12 +50,7 @@ function Cart({ cart, dispatch }) {
               <td>
                 <button
                   type="button"
-                  onClick={() =>
-                    dispatch({
-                      type: 'REMOVE_FROM_CART',
-                      id: product.id,
-                    })
-                  }
+                  onClick={() => removeFromCart(product.id)}
                 >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
@@ -85,4 +82,7 @@ const mapStateToProps = state => ({
   cart: state.cart,
 });
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
