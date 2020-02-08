209950f461c7875c89cc1eaada17aa5d3a18bb20
pedrolssilva <pedrolssilva@hotmail.com>
2020-02-07 21:54:33 -0300

    Calculated total and subprice in mapStateToProps with products info from cart reducer

--- F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/pages/Cart/index.js
+++ F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/pages/Cart/index.js
@@ -9,8 +9,9 @@ import {
 } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
 import * as CartActions from '../../store/modules/cart/actions';
+import { formatPrice } from '../../util/format';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
   function increment(product) {
     updateAmount(product.id, product.amount + 1);
   }
@@ -53,7 +54,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 </div>
               </td>
               <td>
-                <strong>R$ 258,80</strong>
+                <strong>{product.subtotal}</strong>
               </td>
               <td>
                 <button
@@ -71,7 +72,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
         <button type="button">Finalizar pedido</button>
         <Total>
           <span>TOTAL</span>
-          <strong>R$ 1920,28</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
@@ -80,14 +81,26 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 
 Cart.propTypes = {
   cart: PropTypes.arrayOf,
+  total: PropTypes.string,
+  removeFromCart: PropTypes.func.isRequired,
+  updateAmount: PropTypes.func.isRequired,
 };
 
 Cart.defaultProps = {
   cart: [],
+  total: '',
 };
 
 const mapStateToProps = state => ({
-  cart: state.cart,
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
 });
 
 const mapDispatchToProps = dispatch =>
