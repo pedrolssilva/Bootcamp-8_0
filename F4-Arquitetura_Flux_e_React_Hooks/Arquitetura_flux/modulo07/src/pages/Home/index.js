a87e88746b1d131691575b1bcb17d0ba1e7ad0bd
pedrolssilva <pedrolssilva@hotmail.com>
2020-02-10 20:59:09 -0300

    Shown amount into cart to each product available to purchase

--- F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/pages/Home/index.js
+++ F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/pages/Home/index.js
@@ -33,7 +33,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
-
+    const { amount } = this.props;
     return (
       <ProductList>
         {products.map(product => (
@@ -47,7 +47,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#fff" /> 3
+                <MdAddShoppingCart size={16} color="#fff" />{' '}
+                {amount[product.id] || 0}
               </div>
               <span>ADICIONAR AO CARRINHO</span>
             </button>
@@ -60,9 +61,21 @@ class Home extends Component {
 
 Home.propTypes = {
   addToCart: PropType.func.isRequired,
+  amount: PropType.arrayOf,
 };
+
+Home.defaultProps = {
+  amount: {},
+};
+
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
