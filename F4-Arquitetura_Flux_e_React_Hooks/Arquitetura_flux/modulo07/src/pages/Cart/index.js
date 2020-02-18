ef9624f68a6b37b92e1687d3081cf00d311201b5
pedrolssilva <pedrolssilva@hotmail.com>
2020-02-18 20:42:48 -0300

    used redux saga to validate quantity available in stock before increment quantity into cart

--- F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/pages/Cart/index.js
+++ F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/pages/Cart/index.js
@@ -11,13 +11,13 @@ import { Container, ProductTable, Total } from './styles';
 import * as CartActions from '../../store/modules/cart/actions';
 import { formatPrice } from '../../util/format';
 
-function Cart({ cart, total, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
   function increment(product) {
-    updateAmount(product.id, product.amount + 1);
+    updateAmountRequest(product.id, product.amount + 1);
   }
 
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1);
+    updateAmountRequest(product.id, product.amount - 1);
   }
 
   return (
@@ -83,7 +83,7 @@ Cart.propTypes = {
   cart: PropTypes.arrayOf,
   total: PropTypes.string,
   removeFromCart: PropTypes.func.isRequired,
-  updateAmount: PropTypes.func.isRequired,
+  updateAmountRequest: PropTypes.func.isRequired,
 };
 
 Cart.defaultProps = {
--- F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/store/modules/cart/actions.js
+++ F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/modulo07/src/store/modules/cart/actions.js
@@ -19,6 +19,10 @@ export function removeFromCart(id) {
   };
 }
 
-export function updateAmount(id, amount) {
-  return { type: '@cart/UPDATE_AMOUNT', id, amount };
+export function updateAmountRequest(id, amount) {
+  return { type: '@cart/UPDATE_AMOUNT_REQUEST', id, amount };
+}
+
+export function updateAmountSuccess(id, amount) {
+  return { type: '@cart/UPDATE_AMOUNT_SUCCESS', id, amount };
 }
