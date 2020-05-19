c140d5d75ceb0213a811d3cfeea37a33aaada7e2
pedrolssilva <pedrolssilva@hotmail.com>
2020-05-18 21:49:11 -0300

    Setting and using redux-saga to be a middleware between code and reducer handling any validation or request to api

--- F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/desafio07/src/pages/Home/index.js
+++ F4-Arquitetura_Flux_e_React_Hooks/Arquitetura_flux/desafio07/src/pages/Home/index.js
@@ -41,10 +41,9 @@ class Home extends Component {
     this.setState({ products: data });
   };
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
-
-    addToCart(product);
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
+    addToCartRequest(id);
   };
 
   renderProduct = ({ item }) => {
@@ -54,7 +53,7 @@ class Home extends Component {
         <ProductImage source={{ uri: item.image }} />
         <ProductTitle>{item.title}</ProductTitle>
         <ProductPrice>{item.priceFormatted}</ProductPrice>
-        <AddButton onPress={() => this.handleAddProduct(item)}>
+        <AddButton onPress={() => this.handleAddProduct(item.id)}>
           <ProductAmount>
             <Icon name="add-shopping-cart" color="#FFF" size={20} />
             <ProductAmountText>{amount[item.id] || 0}</ProductAmountText>
